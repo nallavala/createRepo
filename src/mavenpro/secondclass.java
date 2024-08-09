66f5b333e6f4e002beece0808517072b77d56046
nallavala <136263300+nallavala@users.noreply.github.com>
2024-08-09 21:24:36 +0530

    Update secondclass.java

--- src/mavenpro/secondclass.java
+++ src/mavenpro/secondclass.java
@@ -4,7 +4,7 @@ public class secondclass {
 
 	public static void main(String[] args) {
 		System.out.println("test");
-		System.out.println("this one may be");
+		System.out.println("test it one may be");
 		/////This is important
 	}
 }
